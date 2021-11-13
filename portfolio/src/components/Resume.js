89d513d2ceba4d6c5907ceabf002755c2bbac7d8
lohith <lohith05amal@gmail.com>
2021-11-13 14:12:12 -0500

    resume attached

---
+++ portfolio/src/components/Resume.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ResumeAmal from "../resume.pdf"
+import { Button } from 'bootstrap'
+const Resume = () => {
+  return (
+    <div>
+      <div>
+      <h1 className="text-uppercase text-center py-5">My Resume <hr /></h1>
+      <div class="text-center">
+      <a className="btn-resume  " href={ResumeAmal} target="_blank"> Download Here</a>
+      </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Resume
