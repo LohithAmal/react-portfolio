e186fedcf496948c34631edbb0de3c9ceeda6fa9
lohith <lohith05amal@gmail.com>
2021-11-13 00:09:43 -0500

    header completed

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import Particle from "react-tsparticles"
 import Navbar from "./components/Navbar"
+import Header from "./components/Header";
 function App() {
   return (
+    <>
+    <Particle
+      params={{
+        particle:{
+          value:30,
+          density:{
+            enable:true,
+            value_area:900
+          }
+        }
+      }}></Particle>
    <Navbar> </Navbar>
+   <Header></Header>
+   </>
   );
 }
 
